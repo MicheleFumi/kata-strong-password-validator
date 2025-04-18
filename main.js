00d6e1b39a7c139ca4b98baa8b5ca06e5cce46b2
MicheleFumi <michele.fumi96@gmail.com>
2025-04-18 10:29:00 +0200

    add upper and strLen validation

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function passwordRetriever(event) {
-  event.preventDefault();
-  console.log(event.target["password"].value);
+function passwordRetriever() {
+  let inputPassword = document.querySelector("#password").value;
+  console.log(inputPassword);
+  const hasUppercase = /[A-Z]/.test(inputPassword);
+
+  if (inputPassword.length < 9) {
+    console.log("testo corto!");
+  }
+  if (!hasUppercase) {
+    console.log("no maiuscole!");
+  }
 }
