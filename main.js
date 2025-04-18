6f92d5e1722b8259be307acefb384c1a35a1da0f
MicheleFumi <michele.fumi96@gmail.com>
2025-04-18 10:47:25 +0200

    added all validation

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ function passwordRetriever() {
   let inputPassword = document.querySelector("#password").value;
   console.log(inputPassword);
   const hasUppercase = /[A-Z]/.test(inputPassword);
-
+  const hasNumber = /[0-9]/.test(inputPassword);
+  const specialChars = /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword);
   if (inputPassword.length < 9) {
     console.log("testo corto!");
   }
   if (!hasUppercase) {
     console.log("no maiuscole!");
+  }
+  if (!hasNumber) {
+    console.log("no numeri!");
+  }
+  if (!specialChars) {
+    console.log("no caratteri speciali!");
   }
 }
