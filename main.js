f0b4c3329c0a16e807bdc460eeb60066d0fd3ff2
MicheleFumi <michele.fumi96@gmail.com>
2025-04-18 11:37:18 +0200

    add classlist

--- main.js
+++ main.js
@@ -4,23 +4,39 @@ function passwordRetriever() {
   const hasUppercase = /[A-Z]/.test(inputPassword);
   const hasNumber = /[0-9]/.test(inputPassword);
   const specialChars = /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword);
-  if (inputPassword.length < 9) {
-    document.querySelector("#not-passed").classList.remove("d-none");
-    document.querySelector("#validation-text").classList.add("text-danger");
+  /* validation fail */
+  if (
+    inputPassword.length < 9 ||
+    !hasUppercase ||
+    !hasNumber ||
+    !specialChars
+  ) {
+    const notPassed = document.getElementsByClassName("not-passed");
+    for (const element of notPassed) {
+      element.classList.remove("d-none");
+    }
+
+    const validationText = document.getElementsByClassName("validation-text");
+    for (const element of validationText) {
+      element.classList.add("text-danger");
+    }
   }
-  if (!hasUppercase) {
-    console.log("no maiuscole!");
-    document.querySelector("#not-passed").classList.remove("d-none");
-    document.querySelector("#validation-text").classList.add("text-danger");
-  }
-  if (!hasNumber) {
-    console.log("no numeri!");
-    document.querySelector("#not-passed").classList.remove("d-none");
-    document.querySelector("#validation-text").classList.add("text-danger");
-  }
-  if (!specialChars) {
-    console.log("no caratteri speciali!");
-    document.querySelector("#not-passed").classList.remove("d-none");
-    document.querySelector("#validation-text").classList.add("text-danger");
+  /* validation success*/
+
+  if (inputPassword.length >= 9 || hasUppercase || hasNumber || specialChars) {
+    const passed = document.getElementsByClassName("passed");
+    for (const element of passed) {
+      element.classList.remove("d-none");
+    }
+    const notPassed = document.getElementsByClassName("not-passed");
+    for (const element of notPassed) {
+      element.classList.add("d-none");
+    }
+
+    const validationText = document.getElementsByClassName("validation-text");
+    for (const element of validationText) {
+      element.classList.remove("text-danger");
+      element.classList.add("text-success");
+    }
   }
 }
