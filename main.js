7fea5941fa98f6c926c0b66518f0245059a234eb
MicheleFumi <michele.fumi96@gmail.com>
2025-04-18 11:00:51 +0200

    add colors to text and icons on validation

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ function passwordRetriever() {
   const hasNumber = /[0-9]/.test(inputPassword);
   const specialChars = /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword);
   if (inputPassword.length < 9) {
-    console.log("testo corto!");
+    document.querySelector("#not-passed").classList.remove("d-none");
+    document.querySelector("#validation-text").classList.add("text-danger");
   }
   if (!hasUppercase) {
     console.log("no maiuscole!");
+    document.querySelector("#not-passed").classList.remove("d-none");
+    document.querySelector("#validation-text").classList.add("text-danger");
   }
   if (!hasNumber) {
     console.log("no numeri!");
+    document.querySelector("#not-passed").classList.remove("d-none");
+    document.querySelector("#validation-text").classList.add("text-danger");
   }
   if (!specialChars) {
     console.log("no caratteri speciali!");
+    document.querySelector("#not-passed").classList.remove("d-none");
+    document.querySelector("#validation-text").classList.add("text-danger");
   }
 }
