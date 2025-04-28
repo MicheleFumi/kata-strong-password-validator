cd03f618b217f8cd767f9b1b678326cb11275b71
MicheleFumi <michele.fumi96@gmail.com>
2025-04-28 09:28:17 +0200

    fix js

--- main.js
+++ main.js
@@ -1,42 +1,93 @@
 function passwordRetriever() {
   let inputPassword = document.querySelector("#password").value;
-  console.log(inputPassword);
+
   const hasUppercase = /[A-Z]/.test(inputPassword);
   const hasNumber = /[0-9]/.test(inputPassword);
-  const specialChars = /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword);
-  /* validation fail */
-  if (
-    inputPassword.length < 9 ||
-    !hasUppercase ||
-    !hasNumber ||
-    !specialChars
-  ) {
-    const notPassed = document.getElementsByClassName("not-passed");
-    for (const element of notPassed) {
-      element.classList.remove("d-none");
-    }
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(inputPassword);
 
-    const validationText = document.getElementsByClassName("validation-text");
-    for (const element of validationText) {
-      element.classList.add("text-danger");
-    }
+  const validationBlocks = document.querySelectorAll(".validation-block");
+
+  // Requisito 1: almeno 9 caratteri
+  if (inputPassword.length >= 9) {
+    validationBlocks[0].querySelector(".passed").classList.remove("d-none");
+    validationBlocks[0].querySelector(".not-passed").classList.add("d-none");
+    validationBlocks[0]
+      .querySelector(".validation-text")
+      .classList.remove("text-danger");
+    validationBlocks[0]
+      .querySelector(".validation-text")
+      .classList.add("text-success");
+  } else {
+    validationBlocks[0].querySelector(".passed").classList.add("d-none");
+    validationBlocks[0].querySelector(".not-passed").classList.remove("d-none");
+    validationBlocks[0]
+      .querySelector(".validation-text")
+      .classList.add("text-danger");
+    validationBlocks[0]
+      .querySelector(".validation-text")
+      .classList.remove("text-success");
   }
-  /* validation success*/
 
-  if (inputPassword.length >= 9 || hasUppercase || hasNumber || specialChars) {
-    const passed = document.getElementsByClassName("passed");
-    for (const element of passed) {
-      element.classList.remove("d-none");
-    }
-    const notPassed = document.getElementsByClassName("not-passed");
-    for (const element of notPassed) {
-      element.classList.add("d-none");
-    }
+  // Requisito 2: almeno una maiuscola
+  if (hasUppercase) {
+    validationBlocks[1].querySelector(".passed").classList.remove("d-none");
+    validationBlocks[1].querySelector(".not-passed").classList.add("d-none");
+    validationBlocks[1]
+      .querySelector(".validation-text")
+      .classList.remove("text-danger");
+    validationBlocks[1]
+      .querySelector(".validation-text")
+      .classList.add("text-success");
+  } else {
+    validationBlocks[1].querySelector(".passed").classList.add("d-none");
+    validationBlocks[1].querySelector(".not-passed").classList.remove("d-none");
+    validationBlocks[1]
+      .querySelector(".validation-text")
+      .classList.add("text-danger");
+    validationBlocks[1]
+      .querySelector(".validation-text")
+      .classList.remove("text-success");
+  }
 
-    const validationText = document.getElementsByClassName("validation-text");
-    for (const element of validationText) {
-      element.classList.remove("text-danger");
-      element.classList.add("text-success");
-    }
+  // Requisito 3: almeno un numero
+  if (hasNumber) {
+    validationBlocks[2].querySelector(".passed").classList.remove("d-none");
+    validationBlocks[2].querySelector(".not-passed").classList.add("d-none");
+    validationBlocks[2]
+      .querySelector(".validation-text")
+      .classList.remove("text-danger");
+    validationBlocks[2]
+      .querySelector(".validation-text")
+      .classList.add("text-success");
+  } else {
+    validationBlocks[2].querySelector(".passed").classList.add("d-none");
+    validationBlocks[2].querySelector(".not-passed").classList.remove("d-none");
+    validationBlocks[2]
+      .querySelector(".validation-text")
+      .classList.add("text-danger");
+    validationBlocks[2]
+      .querySelector(".validation-text")
+      .classList.remove("text-success");
+  }
+
+  // Requisito 4: almeno un carattere speciale
+  if (hasSpecialChar) {
+    validationBlocks[3].querySelector(".passed").classList.remove("d-none");
+    validationBlocks[3].querySelector(".not-passed").classList.add("d-none");
+    validationBlocks[3]
+      .querySelector(".validation-text")
+      .classList.remove("text-danger");
+    validationBlocks[3]
+      .querySelector(".validation-text")
+      .classList.add("text-success");
+  } else {
+    validationBlocks[3].querySelector(".passed").classList.add("d-none");
+    validationBlocks[3].querySelector(".not-passed").classList.remove("d-none");
+    validationBlocks[3]
+      .querySelector(".validation-text")
+      .classList.add("text-danger");
+    validationBlocks[3]
+      .querySelector(".validation-text")
+      .classList.remove("text-success");
   }
 }
